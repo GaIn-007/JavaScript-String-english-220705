ae53712de7f516f14c2c8e18e69b76757147932e
jimmy.wang <jimmy.wang@oocl.com>
2022-07-05 12:13:57 +0800

    string homework

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 // Complete the following procedure, the 'name' variable in all letters to uppercase, output: ' 'HELLO' '.
 var name = 'hello';
 // TODO write your code here...
+name.toUpperCase()
 
 
 // Complete the following procedure, capitalizing all the words in the variable sentence and printing: 'Good Afternoon, Mr Mike.'
 var sentence = 'good afternoon, mr mike.';
 //TODO write your code here...
+sentence.replace(/( |^)[a-z]/g,function (match) {return match.toUpperCase();});
 
 
 // Complete the following procedure to extract the amount of money variable, output: 20.
 var money = '￥20';
 //TODO write your code here...
+money.replace(/\D/, '');
